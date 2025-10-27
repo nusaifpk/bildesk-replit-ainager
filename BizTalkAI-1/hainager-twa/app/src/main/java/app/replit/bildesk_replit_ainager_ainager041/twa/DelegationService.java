fd916da7a67b7f491748c72b86ef3b9d8e127995
nusaifpk <nusaifputhiyakath1@gmail.com>
2025-10-27 10:53:46 +0530

    updated asset SHA256 Fingerprints

---
+++ BizTalkAI-1/hainager-twa/app/src/main/java/app/replit/bildesk_replit_ainager_ainager041/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.replit.bildesk_replit_ainager_ainager041.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
